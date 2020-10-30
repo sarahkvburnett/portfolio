e3df73904c3c1e41159f3238f69076e45671ef66
Sarah-Burnett <burnie8657@gmail.com>
2020-10-30 14:27:40 +0000

    new design

---
+++ js/index.js
@@ -0,0 +1,20 @@
+//toggle dark/light mode on lightbulb click
+const dark = "#000000";
+const light = "#FBFBFB";
+const grey = "#f8f8f8";
+let isDarkMode = true;
+
+document.querySelector(".lightbulb").addEventListener("click", function () {
+	document.querySelector("#lightbulb").classList.toggle("lightbulb-on");
+	if (isDarkMode) {
+		document.documentElement.style.setProperty("--bg", light);
+		document.documentElement.style.setProperty("--color", dark);
+		document.documentElement.style.setProperty("--border", dark);
+		isDarkMode = false;
+	} else {
+		document.documentElement.style.setProperty("--bg", dark);
+		document.documentElement.style.setProperty("--color", light);
+		document.documentElement.style.setProperty("--border", grey);
+		isDarkMode = true;
+	}
+});
